8850034cef001e378d504761f85c09683bac8509
sanaul01 <mdsanaulislam154@gmail.com>
2021-10-08 17:22:00 +0600

    authentication

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCrET2KorQel2Y-SBS1PKQEs24jplTs3Cw",
+    authDomain: "simple-firebase-authenti-715b5.firebaseapp.com",
+    projectId: "simple-firebase-authenti-715b5",
+    storageBucket: "simple-firebase-authenti-715b5.appspot.com",
+    messagingSenderId: "491725875325",
+    appId: "1:491725875325:web:1f384a6771845fc5aae914"
+  };
+
+  export default firebaseConfig;
